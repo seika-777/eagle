c34b0472a4d523bd16d98a0f1db70a5566768490
seika725 <seika.right7@gmail.com>
2025-08-31 22:18:34 +0900

    E1 フォント追加

--- application/frontend/client/const/font/font.tsx
+++ application/frontend/client/const/font/font.tsx
@@ -2,6 +2,7 @@ import {
   Edu_NSW_ACT_Cursive,
   Zen_Kaku_Gothic_New,
   Zen_Maru_Gothic,
+  Zen_Old_Mincho,
 } from "next/font/google";
 
 const eduNswActCursive = Edu_NSW_ACT_Cursive({
@@ -21,5 +22,10 @@ const zenMaruGothic = Zen_Maru_Gothic({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700", "900"],
 });
+const zenOldMincho = Zen_Old_Mincho({
+  variable: "--font-zen-old-mincho",
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+});
 
-export { eduNswActCursive, zenKakuGochicNew, zenMaruGothic };
+export { eduNswActCursive, zenKakuGochicNew, zenMaruGothic, zenOldMincho };
