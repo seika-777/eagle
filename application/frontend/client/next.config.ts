0e8ca8c7c4ee0c58170e7d9210f2e2162a0dc666
seika-777 <seika.right7@gmail.com>
2026-02-15 15:31:05 +0900

    Change-E-2 Next.jsアップデート

--- application/frontend/client/next.config.ts
+++ application/frontend/client/next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
-import path from "path";
 
-const nextConfig: NextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-    prependData: `@import "scss/_const.scss";`
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
